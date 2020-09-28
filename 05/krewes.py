0b491ea4e705d52cb7ebb4f16ed87f5f37e35737
khuang261 <63515253+khuang261@users.noreply.github.com>
2020-09-27 20:31:12 -0400

    Create krewes.py

---
+++ 05/krewes.py
@@ -0,0 +1,15 @@
+#Kelly Huang (with Erin Lee and Pak Ming Lau)
+#SoftDev
+#K05 - Teamwork, but Better this time; Printing a random member from a random team of KREWES
+#2020-09-30
+import random
+
+KREWES = {
+    'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+    'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+    'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+rand_team = random.choice(list(KREWES.keys()))
+team = KREWES[rand_team]
+print(random.choice(team))
