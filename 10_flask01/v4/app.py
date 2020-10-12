85fd78fc9615e42b7f3ce9672f32701f8bb96882
khuang261 <63515253+khuang261@users.noreply.github.com>
2020-10-12 18:14:32 -0400

    Update app.py

--- 10_flask01/v4/app.py
+++ 10_flask01/v4/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev -- Rona Ed.
-# Oct 2020 
+# Team Gummy Bear : Dragos Lup, Kelly Huang
+# SoftDev
+# K10 -- Putting Little Pieces Together
+# 2020-10-12
 
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
@@ -14,3 +15,8 @@ def hello_world():
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+
+#We figured out what debug mode does, we found that when a file gets edited while its being run, and then its saved, debug mode automagically reloads it, instead of having to restart the process.
+
+#This file seemed very similar to the last one, we assumed there was gonna be no difference when ran, because __name__ IS equal to __main__
+#We were right, it was mostly the same, but if the file is imported it doesn't turn on debug mode.
